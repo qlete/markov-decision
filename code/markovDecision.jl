e9d3a8e56ed8d294af22ec2f7272f9df66ebbb0c
Quentin <quentin.lete@gmail.com>
2018-03-06 13:05:49 +0100

    begin file

---
+++ code/markovDecision.jl
@@ -0,0 +1,7 @@
+# Run the MDP value iteration algorithm
+function markovDecision(list::Vector{Int64})::Tuple{Vector{Int64}, Vector{Int64}}
+    println("Hello, world !")
+    return(([1],[2]))
+end
+
+(a,b) = markovDecision([1])
